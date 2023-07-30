691584dbaf503746b1328eba1d2de62d7a471177
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-07-30 01:13:37 -0500

    Add shapes

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ pygame.init()
 
 SIZE_SCREEN = (800, 500);
 
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
 # Creamos ventana
 screen = pygame.display.set_mode(SIZE_SCREEN);
 
@@ -13,3 +18,14 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    
+    # Rellenar el color de fondo
+    screen.fill(WHITE)
+    ## Zona de dibujo
+    pygame.draw.line(screen, GREEN, [0, 100], [0, 200], 10)
+    # (Donde dibujar, color, [inicio], [final], grosor)
+    pygame.draw.rect(screen, BLACK, (100, 100, 80, 80))
+    # (x, y, w, h)
+   
+    # Actualizar pantalla
+    pygame.display.flip()
