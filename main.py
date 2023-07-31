ca8dd169da94bf88c4438a7ede89ce4a4da444ec
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-07-30 20:51:54 -0500

    Add rect shapes with loops

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-
 # Creamos ventana
 screen = pygame.display.set_mode(SIZE_SCREEN);
 
@@ -21,11 +20,17 @@ while True:
     
     # Rellenar el color de fondo
     screen.fill(WHITE)
-    ## Zona de dibujo
+    ## Zona de dibujo --- ##
+    ## Dibujos con for 
+
+    for i in range (100, 700, 100): # (inicio, fin, incremento)
+        pygame.draw.rect(screen, BLACK, (i, 230, 50, 50))
+
     pygame.draw.line(screen, GREEN, [0, 100], [0, 200], 10)
     # (Donde dibujar, color, [inicio], [final], grosor)
     pygame.draw.rect(screen, BLACK, (100, 100, 80, 80))
     # (x, y, w, h)
    
+    ## Fin de Zona de dibujo --- ##
     # Actualizar pantalla
     pygame.display.flip()
